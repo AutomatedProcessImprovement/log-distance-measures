f2e3c36adea0d0d89b8e9eab78f73d930a44095e
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-05-02 16:51:55 +0300

    Add setup and requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='log_similarity_metrics',
+    version='0.1.0',
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'numpy',
+    ]
+)
