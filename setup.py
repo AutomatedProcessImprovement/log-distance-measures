2bd5d68bcab81c88ef6e07707c49035f852233da
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-09-22 16:55:34 +0300

    Upgrade version with DTW metric

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='log_similarity_metrics',
-    version='0.2.0',
+    version='0.3.0',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
