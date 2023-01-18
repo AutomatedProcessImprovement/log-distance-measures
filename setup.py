f4ef9a8dc8cd725132c95f3dbf8f0a550618f365
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-01-18 18:35:10 +0200

    Update version (closes #9)

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name='log_similarity_metrics',
-    version='0.6.0',
+    version='0.7.0',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         'pandas',
+        'scipy',
         'numpy',
+        'jellyfish',
+        'dtw'
     ]
 )
