fbda45f9370a2bda2b8b8a7477adbe05ddf02cbe
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-01-19 15:17:56 +0200

    #10 - Update __init__.py

--- src/log_similarity_metrics/__init__.py
+++ src/log_similarity_metrics/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ["config", "absolute_event_distribution", "circadian_event_distribution", "control_flow_log_distance",
-           "cycle_time_distribution", "n_gram_distribution", "case_arrival_distribution"]
+__all__ = ["relative_event_distribution", "n_gram_distribution", "cycle_time_distribution", "control_flow_log_distance",
+           "circadian_event_distribution", "case_arrival_distribution", "absolute_event_distribution", "config"]
