b636c0e4ecc8adb8c8399014379983d871a3d339
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-02-21 15:49:47 +0200

    Version 0.9.2 with true EMD

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='log_similarity_metrics',
-    version='0.9.1',
+    version='0.9.2',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
