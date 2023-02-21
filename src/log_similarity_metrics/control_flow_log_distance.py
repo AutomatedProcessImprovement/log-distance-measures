978c7838edad7a9436b13c6ef5374676bf97a571
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-02-21 15:26:20 +0200

    #2 Implement parallelized version of CFLD

--- src/log_similarity_metrics/control_flow_log_distance.py
+++ src/log_similarity_metrics/control_flow_log_distance.py
@@ -1,4 +1,7 @@
+import math
+import multiprocessing
 import string
+from concurrent.futures import ProcessPoolExecutor
 from statistics import mean
 
 import numpy as np
@@ -13,7 +16,8 @@ def control_flow_log_distance(
         event_log_1: pd.DataFrame,
         log_1_ids: EventLogIDs,
         event_log_2: pd.DataFrame,
-        log_2_ids: EventLogIDs
+        log_2_ids: EventLogIDs,
+        parallel: bool = False
 ) -> float:
     """
     Compute the Control-Flow Log Distance (see "Camargo M, Dumas M, González-Rojas O. 2021. Discovering generative models
@@ -24,21 +28,16 @@ def control_flow_log_distance(
     :param log_1_ids: mapping for the column IDs of the first event log.
     :param event_log_2: second event log.
     :param log_2_ids: mapping for the column IDs for the second event log.
+    :param parallel: whether to run the distance computation in parallel or in one single core.
 
     :return: the Control-Flow Log Distance measure between [event_log_1] and [event_log_2].
     """
     # Transform the event log to a list of character sequences representing the traces
     sequences_1, sequences_2 = _event_logs_to_activity_sequences(event_log_1, log_1_ids, event_log_2, log_2_ids)
     # Calculate the DL distance between each pair of traces
-    distances = []
-    for i_1, sequence_1 in sequences_1['sequence'].items():
-        for i_2, sequence_2 in sequences_2['sequence'].items():
-            distance = damerau_levenshtein_distance(sequence_1, sequence_2) / max(len(sequence_1), len(sequence_2))
-            distances += [{'i_1': i_1, 'i_2': i_2, 'distance': distance}]
-    distance_matrix = pd.DataFrame(distances).set_index(['i_1', 'i_2'])
+    cost_matrix = _compute_distance_matrix(sequences_1, sequences_2, parallel)
     # Get the optimum pairing
-    cost_matrix = distance_matrix.unstack().to_numpy()
-    row_indexes, col_indexes = linear_sum_assignment(np.array(cost_matrix))
+    row_indexes, col_indexes = linear_sum_assignment(cost_matrix)
     # Compute the Control-Flow Log Distance
     cfld = mean([cost_matrix[i_1, i_2] for i_1, i_2 in zip(row_indexes, col_indexes)])
     # Return the mean of the distances between the traces
@@ -79,7 +78,7 @@ def _event_logs_to_activity_sequences(
     processed_event_log_1 = _event_log_to_activity_sequence(event_log_1, log_1_ids, mapping)
     processed_event_log_2 = _event_log_to_activity_sequence(event_log_2, log_2_ids, mapping)
     # Return value
-    return processed_event_log_1, processed_event_log_2
+    return processed_event_log_1.reset_index(drop=True), processed_event_log_2.reset_index(drop=True)
 
 
 def _event_log_to_activity_sequence(event_log: pd.DataFrame, log_ids: EventLogIDs, mapping: dict) -> pd.DataFrame:
@@ -105,3 +104,55 @@ def _event_log_to_activity_sequence(event_log: pd.DataFrame, log_ids: EventLogID
         ]
     # Return DataFrame with the mapped activity sequences
     return pd.DataFrame(data={'sequence': activity_sequences}, index=case_ids)
+
+
+def _compute_distance_matrix(sequences_1: pd.DataFrame, sequences_2: pd.DataFrame, parallel: bool = False) -> np.ndarray:
+    """
+    Compute the matrix of (string edit) distances between all the sequences in [sequences_1] and [sequences_2].
+
+    :param sequences_1: first list of sequences.
+    :param sequences_2: second list of sequences.
+    :return: matrix of (string edit) distances between each sequence in [sequences_1] and each sequence in [sequences_2].
+    """
+    # Compute distances
+    num_cores = multiprocessing.cpu_count()
+    num_cores = num_cores - 1 if num_cores > 2 else num_cores
+    if parallel and num_cores > 1:
+        # Parallel computation
+        num_traces = len(sequences_1)
+        traces_per_work = math.ceil(num_traces / num_cores)
+        splits = [
+            (i * traces_per_work, min(((i + 1) * traces_per_work), num_traces))
+            for i in range(num_cores) if i * traces_per_work < num_traces
+        ]
+        # Run in parallel
+        with ProcessPoolExecutor(max_workers=num_cores) as pool:
+            args = [
+                (sequences_1[start:end],
+                 sequences_2,
+                 start)
+                for (start, end) in splits
+            ]
+            comparison_results = pool.map(_compare_traces_parallel, args)
+        # Save results
+        distances = [distance for distances in comparison_results for distance in distances]
+    else:
+        # Compute in one thread
+        distances = _compare_traces_parallel((sequences_1, sequences_2, 0))
+    # Create matrix
+    distance_matrix = pd.DataFrame(distances).set_index(['i', 'j'])
+    # Return numpy
+    return distance_matrix.unstack().to_numpy()
+
+
+def _compare_traces_parallel(args) -> list:
+    # Parse args
+    sequences_1, sequences_2, start_index = args
+    # Compute distances
+    distances = []
+    for i, sequence_1 in sequences_1['sequence'].items():
+        for j, sequence_2 in sequences_2['sequence'].items():
+            distance = damerau_levenshtein_distance(sequence_1, sequence_2) / max(len(sequence_1), len(sequence_2))
+            distances += [{'i': i + start_index, 'j': j, 'distance': distance}]
+    # Return computed distances
+    return distances
