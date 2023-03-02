8735715b5402d7104c679b529968957322b3cf49
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-03-02 22:42:20 +0200

    Optimize EMD first subtracting the observations present in both histograms (the mass that is not going to be moved) and adapt tests

--- src/log_similarity_metrics/earth_movers_distance.py
+++ src/log_similarity_metrics/earth_movers_distance.py
@@ -20,29 +20,35 @@ def earth_movers_distance(obs_1: list, obs_2: list, extra_mass: int = 1):
     hist_1, hist_2 = _clean_histograms(obs_1, obs_2)
     total_mass_1, total_mass_2 = sum(hist_1.values()), sum(hist_2.values())
     len_1, len_2 = len(hist_1), len(hist_2)
-    # Create cost matrix
-    c = np.zeros((len_1, len_2))
-    for inx_i, key_i in enumerate(hist_1):
+    if len_1 > 0 and len_2 > 0:
+        # Create cost matrix
+        c = np.zeros((len_1, len_2))
+        for inx_i, key_i in enumerate(hist_1):
+            for idx_j, key_j in enumerate(hist_2):
+                c[inx_i][idx_j] = abs(key_i - key_j)
+        # Create optimization problem
+        model = pulp.LpProblem("EMD", pulp.LpMinimize)
+        # Define variable
+        x = pulp.LpVariable.dicts("x", [(i, j) for i in range(len_1) for j in range(len_2)], lowBound=0, cat="Continuous")
+        # Constraint: the sum of movements for each bin in [hist_1] has to be lower or equal to its mass
+        for inx_i, key_i in enumerate(hist_1):
+            model += pulp.lpSum([x[(inx_i, j)] for j in range(len_2)]) <= hist_1[key_i]
+        # Constraint: the sum of movements for each bin in [hist_2] has to be lower or equal to its mass
         for idx_j, key_j in enumerate(hist_2):
-            c[inx_i][idx_j] = abs(key_i - key_j)
-    # Create optimization problem
-    model = pulp.LpProblem("EMD", pulp.LpMinimize)
-    # Define variable
-    x = pulp.LpVariable.dicts("x", [(i, j) for i in range(len_1) for j in range(len_2)], lowBound=0, cat="Continuous")
-    # Constraint: the sum of movements for each bin in [hist_1] has to be lower or equal to its mass
-    for inx_i, key_i in enumerate(hist_1):
-        model += pulp.lpSum([x[(inx_i, j)] for j in range(len_2)]) <= hist_1[key_i]
-    # Constraint: the sum of movements for each bin in [hist_2] has to be lower or equal to its mass
-    for idx_j, key_j in enumerate(hist_2):
-        model += pulp.lpSum([x[(i, idx_j)] for i in range(len_1)]) <= hist_2[key_j]
-    # Constraint: the total mass moved has to be equal to the minimum of the masses
-    model += pulp.lpSum([x[(i, j)] for j in range(len_2) for i in range(len_1)]) == min(total_mass_1, total_mass_2)
-    # Constraint: minimize the total cost of the movements
-    model += pulp.lpSum([c[i][j] * x[(i, j)] for i in range(len_1) for j in range(len_2)])
-    # Solve problem
-    model.solve()
-    # Return mass + penalization for the extra mass that was not moved
-    return pulp.value(model.objective) + abs(total_mass_1 - total_mass_2) * extra_mass
+            model += pulp.lpSum([x[(i, idx_j)] for i in range(len_1)]) <= hist_2[key_j]
+        # Constraint: the total mass moved has to be equal to the minimum of the masses
+        model += pulp.lpSum([x[(i, j)] for j in range(len_2) for i in range(len_1)]) == min(total_mass_1, total_mass_2)
+        # Constraint: minimize the total cost of the movements
+        model += pulp.lpSum([c[i][j] * x[(i, j)] for i in range(len_1) for j in range(len_2)])
+        # Solve problem
+        model.solve()
+        # Return mass + penalization for the extra mass that was not moved
+        distance = pulp.value(model.objective) + abs(total_mass_1 - total_mass_2) * extra_mass
+    else:
+        # One of them has size 0, compute the total extra mass in the other
+        distance = total_mass_1 * extra_mass + total_mass_2 * extra_mass
+    # Return distance
+    return distance
 
 
 def _clean_histograms(obs_1: list, obs_2: list) -> Tuple[dict, dict]:
@@ -57,15 +63,17 @@ def _clean_histograms(obs_1: list, obs_2: list) -> Tuple[dict, dict]:
     # Transform to 2-D histograms
     hist_1 = Counter(obs_1)
     hist_2 = Counter(obs_2)
-    # Parse [clean_hist_1], adding 0 if the bin is not present, and subtracting the mass that is already in [clean_hist_2]
+    # Parse [hist_1] subtracting the mass that is already in [clean_hist_2]
     clean_hist_1 = {}
-    for i in range(max(obs_1) + 1):
-        if i in hist_1:
-            clean_hist_1[i] = max(hist_1[i] - hist_2.get(i, 0), 0)
-    # Parse [clean_hist_2], adding 0 if the bin is not present, and subtracting the mass that is already in [clean_hist_1]
+    for i in hist_1:
+        intersection_value = max(hist_1[i] - hist_2.get(i, 0), 0)
+        if intersection_value > 0:
+            clean_hist_1[i] = intersection_value
+    # Parse [hist_2] subtracting the mass that is already in [clean_hist_1]
     clean_hist_2 = {}
-    for i in range(max(obs_2) + 1):
-        if i in hist_2:
-            clean_hist_2[i] = max(hist_2[i] - hist_1.get(i, 0), 0)
+    for i in hist_2:
+        intersection_value = max(hist_2[i] - hist_1.get(i, 0), 0)
+        if intersection_value > 0:
+            clean_hist_2[i] = intersection_value
     # Return clean histograms
     return clean_hist_1, clean_hist_2
