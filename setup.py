a3e513cdc70c85c60a88f1e6e970af80eadc7667
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-03-02 22:27:45 +0200

    Reimplement EMD as an LP problem optimizing the movements w.r.t. the bins, not the observations

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='log_similarity_metrics',
-    version='0.9.2',
+    version='0.9.3',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
@@ -10,6 +10,7 @@ setup(
         'scipy',
         'numpy',
         'jellyfish',
+        'pulp',
         'dtw'
     ]
 )
--- src/log_similarity_metrics/earth_movers_distance.py
+++ src/log_similarity_metrics/earth_movers_distance.py
@@ -2,73 +2,70 @@ from collections import Counter
 from typing import Tuple
 
 import numpy as np
-from scipy.optimize import linear_sum_assignment
+import pulp as pulp
 
 
-def earth_movers_distance(hist_1: list, hist_2: list, extra_mass: float = 1.0) -> float:
+def earth_movers_distance(obs_1: list, obs_2: list, extra_mass: int = 1):
     """
     Compute the Earth Mover's Distance (EMD) between two histograms given the 1D array of observations. The EMD corresponds to the amount of
     observations that have to be moved (multiplied by the distance of the movement) to transform one histogram into the other. If one of the
     histograms has more observations than the other, each extra observation is penalized by [extra_mass].
 
-    :param hist_1: 1D array with the observations of histogram 1.
-    :param hist_2: 1D array with the observations of histogram 2.
+    :param obs_1: 1D array with the observations of histogram 1.
+    :param obs_2: 1D array with the observations of histogram 2.
     :param extra_mass: Penalization for extra observation.
     :return: The Earth Mover's Distance (EMD) between [hist_1] and [hist_2].
     """
-    # Remove similar observations
-    hist_1, hist_2 = _clean_histograms(hist_1, hist_2)
-    if len(hist_1) == len(hist_2):
-        # Sort
-        hist_1.sort()
-        hist_2.sort()
-        # Compute distance as the sum of absolute movements element by element
-        distance = 0
-        for i in range(len(hist_1)):
-            distance += abs(hist_1[i] - hist_2[i])
-    else:
-        # Swipe them if [hist_1] is smaller than [hist_2], so [hist_1] is always the larger histogram (or equal size)
-        hist_1, hist_2 = (hist_2, hist_1) if len(hist_1) < len(hist_2) else (hist_1, hist_2)
-        # Create cost matrix
-        cost_matrix = np.zeros((len(hist_1), len(hist_1)))
-        # Cost of movement of each observation in [hist_1] to each other observation in [hist_2]
-        for i in range(len(hist_1)):
-            for j in range(len(hist_2)):
-                # Distance of moving observation [i] from [hist_1] to observation [j] from [hist_2]
-                cost_matrix[i][j] = abs(hist_1[i] - hist_2[j])
-            for j in range(len(hist_1) - len(hist_2)):
-                # Distance of considering observation [i] from [hist_1] as extra observation (deletion)
-                cost_matrix[i][j + len(hist_2)] = extra_mass
-        # Compute combination of movements that minimize the total cost
-        row_ind, col_ind = linear_sum_assignment(cost_matrix)
-        # Compute distance
-        distance = cost_matrix[row_ind, col_ind].sum()
-    # Return distance
-    return distance
+    # Transform the 1-D histograms to 2-D histograms removing similar values
+    hist_1, hist_2 = _clean_histograms(obs_1, obs_2)
+    total_mass_1, total_mass_2 = sum(hist_1.values()), sum(hist_2.values())
+    len_1, len_2 = len(hist_1), len(hist_2)
+    # Create cost matrix
+    c = np.zeros((len_1, len_2))
+    for inx_i, key_i in enumerate(hist_1):
+        for idx_j, key_j in enumerate(hist_2):
+            c[inx_i][idx_j] = abs(key_i - key_j)
+    # Create optimization problem
+    model = pulp.LpProblem("EMD", pulp.LpMinimize)
+    # Define variable
+    x = pulp.LpVariable.dicts("x", [(i, j) for i in range(len_1) for j in range(len_2)], lowBound=0, cat="Continuous")
+    # Constraint: the sum of movements for each bin in [hist_1] has to be lower or equal to its mass
+    for inx_i, key_i in enumerate(hist_1):
+        model += pulp.lpSum([x[(inx_i, j)] for j in range(len_2)]) <= hist_1[key_i]
+    # Constraint: the sum of movements for each bin in [hist_2] has to be lower or equal to its mass
+    for idx_j, key_j in enumerate(hist_2):
+        model += pulp.lpSum([x[(i, idx_j)] for i in range(len_1)]) <= hist_2[key_j]
+    # Constraint: the total mass moved has to be equal to the minimum of the masses
+    model += pulp.lpSum([x[(i, j)] for j in range(len_2) for i in range(len_1)]) == min(total_mass_1, total_mass_2)
+    # Constraint: minimize the total cost of the movements
+    model += pulp.lpSum([c[i][j] * x[(i, j)] for i in range(len_1) for j in range(len_2)])
+    # Solve problem
+    model.solve()
+    # Return mass + penalization for the extra mass that was not moved
+    return pulp.value(model.objective) + abs(total_mass_1 - total_mass_2) * extra_mass
 
 
-def _clean_histograms(hist_1: list, hist_2: list) -> Tuple[list, list]:
+def _clean_histograms(obs_1: list, obs_2: list) -> Tuple[dict, dict]:
     """
-    Remove from two histograms the observations that appear in both of them.
+    Transform two 1-D histograms (list of observations) to two 2-D histograms without the observations that they have in common.
 
-    :param hist_1: histogram 1.
-    :param hist_2: histogram 2.
+    :param obs_1: 1-D histogram 1.
+    :param obs_2: 1-D histogram 2.
 
-    :return: both histograms without the common observations.
+    :return: both histograms in 2-D space, without the common observations.
     """
-    # Get frequency of each observation
-    hist_1_freq = Counter(hist_1)
-    hist_2_freq = Counter(hist_2)
-    # Create list without elements present in the other list (considering duplicates)
-    clean_hist_1 = [
-        element
-        for i in hist_1_freq
-        for element in [i] * max(hist_1_freq[i] - hist_2_freq.get(i, 0), 0)
-    ]
-    clean_hist_2 = [
-        element
-        for i in hist_2_freq
-        for element in [i] * max(hist_2_freq[i] - hist_1_freq.get(i, 0), 0)
-    ]
+    # Transform to 2-D histograms
+    hist_1 = Counter(obs_1)
+    hist_2 = Counter(obs_2)
+    # Parse [clean_hist_1], adding 0 if the bin is not present, and subtracting the mass that is already in [clean_hist_2]
+    clean_hist_1 = {}
+    for i in range(max(obs_1) + 1):
+        if i in hist_1:
+            clean_hist_1[i] = max(hist_1[i] - hist_2.get(i, 0), 0)
+    # Parse [clean_hist_2], adding 0 if the bin is not present, and subtracting the mass that is already in [clean_hist_1]
+    clean_hist_2 = {}
+    for i in range(max(obs_2) + 1):
+        if i in hist_2:
+            clean_hist_2[i] = max(hist_2[i] - hist_1.get(i, 0), 0)
     # Return clean histograms
     return clean_hist_1, clean_hist_2
